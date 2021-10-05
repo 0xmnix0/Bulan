cd1beb317e209cccee684905eac058ef2c7f9937
Soleh2 <rifando.p.p0@gmail.com>
2021-10-05 23:55:51 +0700

    Bulan

---
+++ message/help.js
@@ -0,0 +1,228 @@
+exports.gameMenu = (prefix) => {
+    return `
+━━━「 *GAME* 」━━━
+> ${prefix}slot
+> ${prefix}limitgame
+> ${prefix}gelud *@tag*
+> ${prefix}tictactoe *@tag*
+> ${prefix}siapaaku
+> ${prefix}family100
+> ${prefix}kuismath
+> ${prefix}asahotak
+> ${prefix}tebaklirik
+> ${prefix}tebaklagu
+> ${prefix}tebakkata
+> ${prefix}susunkata
+> ${prefix}kimiakuis
+> ${prefix}caklontong
+> ${prefix}tebakjenaka
+> ${prefix}tebakanime
+> ${prefix}tebaktebakan
+> ${prefix}tebakgambar
+> ${prefix}tebakbendera
+> ${prefix}suit *batu/kertas/gunting*
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+ ━━━ 「 *FUN MENU* 」 ━━━
+> ${prefix}mining
+> ${prefix}togel
+> ${prefix}cekwatak
+> ${prefix}cekmati
+> ${prefix}wangy *nama*
+> ${prefix}citacita
+> ${prefix}toxic
+> ${prefix}truth
+> ${prefix}dare
+> ${prefix}apakah
+> ${prefix}bisakah
+> ${prefix}kapankah
+> ${prefix}rate
+> ${prefix}jadian
+> ${prefix}cantik
+> ${prefix}ganteng
+> ${prefix}beban
+> ${prefix}babi
+> ${prefix}cekganteng
+> ${prefix}cekcantik
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+━━━ 「 *OTHER MENU* 」 ━━━
+> ${prefix}brainly *query*
+> ${prefix}shopee *product*
+> ${prefix}playstore *query*
+> ${prefix}ssweb *query*
+> ${prefix}google *query*
+> ${prefix}image *query*
+> ${prefix}pinterest *query*
+> ${prefix}nulis *teks*
+> ${prefix}iguser *ussername*
+> ${prefix}igstalk *username*
+> ${prefix}githubstalk *username*
+> ${prefix}tiktokstalk *ussername*
+> ${prefix}img2url *reply foto*
+> ${prefix}ytsearch *query*
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+━━━「 *STICKER* 」━━━
+> ${prefix}dadu
+> ${prefix}doge
+> ${prefix}toimg
+> ${prefix}patrick
+> ${prefix}garwgura
+> ${prefix}ttg 
+> ${prefix}attp
+> ${prefix}stickeranim
+> ${prefix}semoji *emoji*
+> ${prefix}sticker 
+> ${prefix}smeme *teks|teks*
+> ${prefix}swm
+> ${prefix}take
+> ${prefix}tovideo *reply sgif*
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+━━━「 *WIBU* 」━━━
+> ${prefix}loli
+> ${prefix}manga
+> ${prefix}anime
+> ${prefix}lolivideo
+> ${prefix}husbu
+> ${prefix}waifu
+> ${prefix}milf
+> ${prefix}neko
+> ${prefix}kanna
+> ${prefix}sagiri
+> ${prefix}hentai
+> ${prefix}cosplay
+> ${prefix}wallnime
+> ${prefix}kusonime
+> ${prefix}megumin
+> ${prefix}otakudesu
+> ${prefix}doujindesu
+> ${prefix}storyanime
+> ${prefix}nakanomiku
+> ${prefix}nakanonino
+> ${prefix}nakanoitsuki
+> ${prefix}otakuongoing
+> ${prefix}nhentai *code*
+> ${prefix}nekopoi *link*
+> ${prefix}nekopoi3d
+> ${prefix}nekopoicosplay
+> ${prefix}nekopoisearch
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+━━━「 *OWNER ONLY* 」━━━
+> ${prefix}bc *teks*
+> ${prefix}term
+> ${prefix}join *link gc*
+> ${prefix}eval
+> ${prefix}reset
+> ${prefix}clearall
+> ${prefix}getquoted
+> ${prefix}premium add *@tag|nomor*
+> ${prefix}shutdown
+> ${prefix}addupdate *fiturnya*
+> ${prefix}leaveall
+> ${prefix}sewa *add/del waktunya*
+> ${prefix}exif *nama|author*
+> ${prefix}premium del *@tag|nomor*
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+━━━「 *GRUP ONLY* 」━━━
+> ${prefix}add
+> ${prefix}kick
+> ${prefix}promote
+> ${prefix}demote
+> ${prefix}leave
+> ${prefix}tagall
+> ${prefix}grup
+> ${prefix}setppgrup
+> ${prefix}antilink
+> ${prefix}welcome
+> ${prefix}setdesc
+> ${prefix}linkgroup
+> ${prefix}listonline
+> ${prefix}mute
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+━━━「 *DOWNLOADER* 」━━━
+> ${prefix}facebook
+> ${prefix}nthentaipdf
+> ${prefix}tiktok
+> ${prefix}twitter
+> ${prefix}tiktoknowm
+> ${prefix}igdl
+> ${prefix}ytmp3
+> ${prefix}ytmp4
+> ${prefix}mediafire
+> ${prefix}soundcloud
+> ${prefix}tikokaudio
+> ${prefix}play
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+
+━━━「 *RULES* 」━━━
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+━━━「 *INFO* 」━━━
+> ${prefix}update
+> ${prefix}level
+> ${prefix}listsewa
+> ${prefix}sourcecode
+> ${prefix}profile
+> ${prefix}waktu
+> ${prefix}botstat
+> ${prefix}rules
+> ${prefix}sewabot
+> ${prefix}leaderboard
+> ${prefix}owner
+> ${prefix}ping
+> ${prefix}runtime
+> ${prefix}donasi
+> ${prefix}listpremium
+> ${prefix}cekpremium
+> ${prefix}bugreport *keluhan*
+
+
+`
+}
+exports.sewamenu = (prefix) => {
+return `
+━━━「 *PRICE LIST* 」━━━
+GA ADA
+
+`
+}
+exports.infoowner = (prefix) => {
+return `
+Ngopi Bang☕
+
+`
+}
---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'waiting for your request we are processing',
+			success: 'Succeed',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'Sorry an error occurred',
+				stick: 'Not a sticker',
+				Iv: 'Invalid link'
+			},
+			only: {
+				group: 'This command is only for Groups',
+				admin: 'This command is only for Admin',
+				premium: 'This command is only for Premium users',
+				owner: 'This command is only for Owner',
+				Badmin: 'This command can be used when the Bot becomes Admin',
+			}
+		}
